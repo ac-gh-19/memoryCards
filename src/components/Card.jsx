f6eb497043fd416ac4a5b0358f40cedf4103ce29
ac-gh-19 <chanandrew1925@gmail.com>
2025-11-17 03:18:51 -0800

    fix: tweak some card styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 export default function Card({pokemon, handleClick}) {
     const { name, image } = pokemon;
     return (
-        <div className="border flex flex-col  items-center justify-center">
-            <img src={image}/>
-            <div>{name}</div>
+        <div className="border flex flex-col  items-center justify-center rounded-2xl p-10">
+            <img src={image} className="w-44"/>
+            <div className="text-xl">{name[0].toUpperCase() + name.slice(1)}</div>
         </div>
     )
 }
