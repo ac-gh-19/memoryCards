6331878865cef089fdfcb479f410b6bdd61a7136
ac-gh-19 <chanandrew1925@gmail.com>
2025-11-17 03:28:54 -0800

    feat: add score update / check for double click

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,23 @@ function App() {
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0)
   const [pokemons, setPokemons] = useState(null);
+  const [clickedPokemonIDS, setClickedPokemonIDS] = useState(new Set())
 
+  function handleCardClick(pokemonID) {
+    if (clickedPokemonIDS.has(pokemonID)) {
+      setCurrentScore(0);
+      setPokemons(null);
+      return;
+    }
+
+    const newClickedPokemonIDS = new Set(clickedPokemonIDS);
+    newClickedPokemonIDS.add(pokemonID);
+    setClickedPokemonIDS(newClickedPokemonIDS);
+    setCurrentScore(currentScore + 1);
+    if (currentScore + 1 > bestScore) {
+      setBestScore(bestScore + 1);
+    }
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -40,7 +56,7 @@ function App() {
       style={{gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))"}}>
         { pokemons != null ? 
         pokemons.map(pokemon => {
-          return <Card key={pokemon.id} pokemon={pokemon}></Card>
+          return <Card key={pokemon.id} pokemon={pokemon} onClick={handleCardClick}></Card>
         })
        : <div> LOADING </div> }
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,8 @@
-export default function Card({pokemon, handleClick}) {
+export default function Card({pokemon, onClick}) {
     const { name, image } = pokemon;
     return (
-        <div className="border flex flex-col  items-center justify-center rounded-2xl p-10">
+        <div className="border flex flex-col  items-center justify-center rounded-2xl p-10"
+        onClick={() => onClick(pokemon.id)}>
             <img src={image} className="w-44"/>
             <div className="text-xl">{name[0].toUpperCase() + name.slice(1)}</div>
         </div>
